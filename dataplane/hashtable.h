1ea957676e8d08c9d6ab246528d9acc58ca59cf5
Timur Aitov <taitov@yandex-team.ru>
2024-02-14 11:35:31 +0300

    TEST

--- dataplane/hashtable.h
+++ dataplane/hashtable.h
@@ -1771,17 +1771,44 @@ public:
 	constexpr static uint32_t mask_full = 0xFFFFFFFFu;
 	constexpr static uint32_t shift_valid = (32 - 1 - valid_bit_offset);
 	constexpr static uint64_t longest_chain_size = chunk_size;
+	constexpr static uint32_t pairs_size_min = 128;
 
 	static_assert(__builtin_popcount(chunk_size) == 1);
 	static_assert(valid_bit_offset < 32);
 
-public:
-	hashtable_mod_id32_dynamic(const uint32_t keys_size)
+	struct stats_t
 	{
-		total_mask = keys_size - 1;
+		uint32_t pairs_count;
+		std::array<uint32_t, chunk_size + 1> pairs_in_chunks;
+		uint32_t longest_chain;
+		uint64_t insert_failed;
+		uint64_t rewrites;
+	};
 
+	static uint64_t calculate_sizeof(const uint32_t pairs_size)
+	{
+		if (!pairs_size)
+		{
+			YANET_LOG_ERROR("wrong pairs_size: %u\n", pairs_size);
+			return 0;
+		}
+
+		if (__builtin_popcount(pairs_size) != 1)
+		{
+			YANET_LOG_ERROR("wrong pairs_size: %u is non power of 2\n", pairs_size);
+			return 0;
+		}
+
+		return sizeof(hashtable_t) + pairs_size * sizeof(pair);
+	}
+
+public:
+	hashtable_mod_id32_dynamic(const uint32_t pairs_size) :
+	        total_mask(pairs_size - 1),
+	        pairs_size(pairs_size)
+	{
 		for (uint32_t i = 0;
-		     i < keys_size;
+		     i < pairs_size;
 		     i++)
 		{
 			pairs[i].value = 0;
@@ -1867,10 +1894,92 @@ public:
 		return mask;
 	}
 
-protected:
-	template<typename, uint32_t, unsigned int>
-	friend class updater_hashtable_mod_id32;
+	eResult fill(stats_t& stats, const std::vector<std::tuple<key_t, uint32_t>>& pairs)
+	{
+		eResult result = eResult::success;
 
+		stats.pairs_count = 0;
+		stats.pairs_in_chunks.fill(0);
+		stats.longest_chain = 0;
+		stats.insert_failed = 0;
+		stats.rewrites = 0;
+
+		for (const auto& [key, value] : pairs)
+		{
+			eResult insert_result = insert(stats, key, value);
+			if (insert_result != eResult::success)
+			{
+				result = insert_result;
+			}
+		}
+
+		for (uint32_t chunk_i = 0;
+		     chunk_i < pairs_size / chunk_size;
+		     chunk_i++)
+		{
+			unsigned int count = 0;
+
+			for (uint32_t pair_i = 0;
+			     pair_i < chunk_size;
+			     pair_i++)
+			{
+				if (is_valid(chunk_i * chunk_size + pair_i))
+				{
+					count++;
+				}
+			}
+
+			stats.pairs_in_chunks[count]++;
+		}
+
+		return result;
+	}
+
+	eResult insert(stats_t& stats,
+	               const key_t& key,
+	               const uint32_t value)
+	{
+		const uint32_t hash = calculate_hash(key) & total_mask;
+
+		for (unsigned int try_i = 0;
+		     try_i < chunk_size;
+		     try_i++)
+		{
+			const uint32_t index = (hash + try_i) & total_mask;
+
+			if (!is_valid(index))
+			{
+				memcpy(&pairs[index].key, &key, sizeof(key_t));
+				pairs[index].value = value;
+				pairs[index].value |= 1u << shift_valid;
+
+				stats.pairs_count++;
+
+				uint64_t longest_chain = try_i + 1;
+				if (stats.longest_chain < longest_chain)
+				{
+					stats.longest_chain = longest_chain;
+				}
+
+				return eResult::success;
+			}
+			else if (is_valid_and_equal(index, key))
+			{
+				pairs[index].value = value;
+				pairs[index].value |= 1u << shift_valid;
+
+				stats.rewrites++;
+
+				return eResult::success;
+			}
+		}
+
+		stats.insert_failed++;
+
+		return eResult::isFull;
+	}
+
+protected:
 	inline bool is_valid(const uint32_t index) const
 	{
 		return (pairs[index].value >> shift_valid) & 1;
@@ -1888,6 +1997,7 @@ protected:
 
 protected:
 	uint32_t total_mask;
+	uint32_t pairs_size;
 
 	struct pair
 	{
--- dataplane/updater.h
+++ dataplane/updater.h
@@ -12,6 +12,22 @@
 namespace dataplane
 {
 
+[[maybe_unused]] static uint32_t upper_power_of_two(const uint32_t value)
+{
+	/// @todo: use __builtin_clz
+
+	uint32_t result = 1;
+	while (result < value)
+	{
+		result <<= 1;
+		if (!result)
+		{
+			return 0;
+		}
+	}
+	return result;
+}
+
 /// XXX: MOVE
 namespace limits
 {
@@ -344,11 +360,12 @@ public:
 
 template<typename key_t,
          uint32_t chunk_size,
-         unsigned int valid_bit_offset = 0>
+         unsigned int valid_bit_offset = 0,
+         hash_function_t<key_t> calculate_hash = calculate_hash_crc<key_t>>
 class updater_hashtable_mod_id32
 {
 public:
-	using object_type = hashtable_mod_id32_dynamic<key_t, chunk_size, valid_bit_offset>;
+	using object_type = hashtable_mod_id32_dynamic<key_t, chunk_size, valid_bit_offset, calculate_hash>;
 
 	updater_hashtable_mod_id32(const char* name,
 	                           dataplane::memory_manager* memory_manager,
@@ -356,65 +373,33 @@ public:
 	        name(name),
 	        memory_manager(memory_manager),
 	        socket_id(socket_id),
-	        keys_count(0),
-	        keys_size(0),
+	        pairs_size(0),
 	        pointer(nullptr)
 	{
 	}
 
-	static uint64_t calculate_sizeof(const uint64_t keys_size)
-	{
-		if (!keys_size)
-		{
-			YANET_LOG_ERROR("wrong keys_size: %lu\n", keys_size);
-			return 0;
-		}
-		else if (keys_size > 0xFFFFFFFFull)
-		{
-			YANET_LOG_ERROR("wrong keys_size: %lu\n", keys_size);
-			return 0;
-		}
-
-		if (__builtin_popcount(keys_size) != 1)
-		{
-			YANET_LOG_ERROR("wrong keys_size: %lu is non power of 2\n", keys_size);
-			return 0;
-		}
-
-		return sizeof(object_type) + keys_size * sizeof(typename object_type::pair);
-	}
-
 	eResult update(const std::vector<std::tuple<key_t, uint32_t>>& values)
 	{
-		constexpr uint64_t keys_size_min = 128;
-
-		keys_size = 1;
-		{
-			uint64_t keys_size_need = std::max(keys_size_min,
-			                                   (uint64_t)(4ull * values.size()));
-			while (keys_size < keys_size_need)
-			{
-				keys_size <<= 1;
-			}
-		}
+		pairs_size = upper_power_of_two(std::max(object_type::pairs_size_min,
+		                                         (uint32_t)(4ull * values.size())));
 
 		for (;;)
 		{
 			pointer = memory_manager->create<object_type>(name.data(),
 			                                              socket_id,
-			                                              calculate_sizeof(keys_size),
-			                                              keys_size);
+			                                              object_type::calculate_sizeof(pairs_size),
+			                                              pairs_size);
 			if (pointer == nullptr)
 			{
 				return eResult::errorAllocatingMemory;
 			}
 
-			eResult result = fill(values);
+			eResult result = pointer->fill(stats, values);
 			if (result != eResult::success)
 			{
 				/// try again
-
-				keys_size <<= 1;
+				clear();
+				pairs_size *= 2;
 				continue;
 			}
 
@@ -437,113 +422,28 @@ public:
 	{
 		limits.emplace_back(name + ".keys",
 		                    socket_id,
-		                    keys_count,
-		                    keys_size);
+		                    stats.pairs_count,
+		                    pairs_size);
 		limits.emplace_back(name + ".longest_collision",
 		                    socket_id,
-		                    longest_chain,
+		                    stats.longest_chain,
 		                    chunk_size);
 	}
 
 	void report(nlohmann::json& report) const
 	{
 		report["pointer"] = (uint64_t)pointer; ///< XXX: TO_HEX
-		report["keys_count"] = keys_count;
-		report["keys_size"] = keys_size;
+		report["pairs_count"] = stats.pairs_count;
+		report["pairs_size"] = pairs_size;
 		for (unsigned int i = 0;
-		     i < keys_in_chunks.size();
+		     i < stats.pairs_in_chunks.size();
 		     i++)
 		{
-			report["keys_in_chunks"][i] = keys_in_chunks[i];
+			report["pairs_in_chunks"][i] = stats.pairs_in_chunks[i];
 		}
-		report["longest_chain"] = longest_chain;
-		report["insert_failed"] = insert_failed;
-		report["rewrites"] = rewrites;
-	}
-
-protected:
-	eResult fill(const std::vector<std::tuple<key_t, uint32_t>>& values)
-	{
-		eResult result = eResult::success;
-
-		keys_count = 0;
-		keys_in_chunks.fill(0);
-		longest_chain = 0;
-		insert_failed = 0;
-		rewrites = 0;
-
-		for (const auto& [key, value] : values)
-		{
-			eResult insert_result = insert(key, value);
-			if (insert_result != eResult::success)
-			{
-				result = insert_result;
-			}
-		}
-
-		for (uint32_t chunk_i = 0;
-		     chunk_i < keys_size / chunk_size;
-		     chunk_i++)
-		{
-			unsigned int count = 0;
-
-			for (uint32_t pair_i = 0;
-			     pair_i < chunk_size;
-			     pair_i++)
-			{
-				if (pointer->is_valid(chunk_i * chunk_size + pair_i))
-				{
-					count++;
-				}
-			}
-
-			keys_in_chunks[count]++;
-		}
-
-		return result;
-	}
-
-	eResult insert(const key_t& key,
-	               const uint32_t value)
-	{
-		const uint32_t hash = object_type::calculate_hash(key) & pointer->total_mask;
-
-		for (unsigned int try_i = 0;
-		     try_i < chunk_size;
-		     try_i++)
-		{
-			const uint32_t index = (hash + try_i) & pointer->total_mask;
-
-			if (!pointer->is_valid(index))
-			{
-				memcpy(&pointer->pairs[index].key, &key, sizeof(key_t));
-				pointer->pairs[index].value = value;
-				pointer->pairs[index].value |= 1u << object_type::shift_valid;
-
-				keys_count++;
-
-				uint64_t longest_chain = try_i + 1;
-				if (this->longest_chain < longest_chain)
-				{
-					this->longest_chain = longest_chain;
-				}
-
-				return eResult::success;
-			}
-			else if (pointer->is_valid_and_equal(index, key))
-			{
-				pointer->pairs[index].value = value;
-				pointer->pairs[index].value |= 1u << object_type::shift_valid;
-
-				rewrites++;
-
-				return eResult::success;
-			}
-		}
-
-		insert_failed++;
-
-		return eResult::isFull;
+		report["longest_chain"] = stats.longest_chain;
+		report["insert_failed"] = stats.insert_failed;
+		report["rewrites"] = stats.rewrites;
 	}
 
 protected:
@@ -551,209 +451,11 @@ protected:
 	dataplane::memory_manager* memory_manager;
 	tSocketId socket_id;
 
-	uint32_t keys_count;
-	uint32_t keys_size;
-	std::array<uint32_t, chunk_size + 1> keys_in_chunks;
-	uint32_t longest_chain;
-	uint64_t insert_failed;
-	uint64_t rewrites;
+	uint32_t pairs_size;
+	typename object_type::stats_t stats;
 
 public:
 	object_type* pointer;
-};
-
-template<typename key_t,
-         uint32_t chunk_size,
-         unsigned int valid_bit_offset = 0>
-class updater123321123_hashtable_mod_id32 : public updater_base
-{
-public:
-	using hashtable_t = hashtable_mod_id32_dynamic<key_t, chunk_size, valid_bit_offset>;
-
-	hashtable_t* get_pointer()
-	{
-		return reinterpret_cast<hashtable_t*>(updater_base::get_pointer());
-	}
-
-	const hashtable_t* get_pointer() const
-	{
-		return reinterpret_cast<const hashtable_t*>(updater_base::get_pointer());
-	}
-
-	template<typename update_key_t>
-	eResult update(const std::vector<std::tuple<update_key_t, uint32_t>>& keys)
-	{
-		eResult result = eResult::success;
-
-		uint64_t keys_count = 1;
-		{
-			constexpr uint64_t keys_size_min = 128;
-
-			uint64_t keys_size = std::max(keys_size_min,
-			                              (uint64_t)(4ull * keys.size()));
-			while (keys_count < keys_size)
-			{
-				keys_count <<= 1;
-			}
-		}
-
-		for (;;)
-		{
-			YANET_LOG_INFO("XXX:UPDATE:1\n");
-			result = alloc(hashtable_t::calculate_sizeof(keys_count));
-			YANET_LOG_INFO("XXX:UPDATE:2\n");
-			if (result != eResult::success)
-			{
-				return result;
-			}
-
-			get_pointer()->total_mask = keys_count - 1;
-			total_size = keys_count;
-
-			YANET_LOG_INFO("XXX:UPDATE:3\n");
-			for (uint32_t i = 0;
-			     i < total_size;
-			     i++)
-			{
-				get_pointer()->pairs[i].value = 0;
-			}
-			YANET_LOG_INFO("XXX:UPDATE:4\n");
-
-			result = fill(keys);
-			YANET_LOG_INFO("XXX:UPDATE:5\n");
-			if (result != eResult::success)
-			{
-				/// try again
-
-				keys_count <<= 1;
-				continue;
-			}
-
-			break;
-		}
-
-		return eResult::success;
-	}
-
-	template<typename list_T> ///< @todo: common::idp::limits::response
-	void limits(list_T& list) const
-	{
-		list.emplace_back(name + ".keys",
-		                  socket_id,
-		                  keys_count,
-		                  total_size);
-		list.emplace_back(name + ".longest_collision",
-		                  socket_id,
-		                  longest_chain,
-		                  chunk_size);
-	}
-
-protected:
-	template<typename update_key_t>
-	eResult fill(const std::vector<std::tuple<update_key_t, uint32_t>>& keys)
-	{
-		auto* hashtable = get_pointer();
-
-		eResult result = eResult::success;
-
-		keys_count = 0;
-		keys_in_chunks.fill(0);
-		longest_chain = 0;
-		insert_failed = 0;
-		rewrites = 0;
-
-		for (const auto& [key, value] : keys)
-		{
-			eResult insert_result;
-			if constexpr (std::is_same_v<update_key_t, key_t>)
-			{
-				insert_result = insert(key, value);
-			}
-			else
-			{
-				insert_result = insert(key_t::convert(key), value);
-			}
-
-			if (insert_result != eResult::success)
-			{
-				result = insert_result;
-			}
-		}
-
-		for (uint32_t chunk_i = 0;
-		     chunk_i < total_size / chunk_size;
-		     chunk_i++)
-		{
-			unsigned int count = 0;
-
-			for (uint32_t pair_i = 0;
-			     pair_i < chunk_size;
-			     pair_i++)
-			{
-				if (hashtable->is_valid(chunk_i * chunk_size + pair_i))
-				{
-					count++;
-				}
-			}
-
-			keys_in_chunks[count]++;
-		}
-
-		return result;
-	}
-
-	eResult insert(const key_t& key,
-	               const uint32_t value)
-	{
-		auto* hashtable = get_pointer();
-
-		const uint32_t hash = hashtable_t::calculate_hash(key) & hashtable->total_mask;
-
-		for (unsigned int try_i = 0;
-		     try_i < chunk_size;
-		     try_i++)
-		{
-			const uint32_t index = (hash + try_i) % total_size;
-
-			if (!hashtable->is_valid(index))
-			{
-				memcpy(&hashtable->pairs[index].key, &key, sizeof(key_t));
-				hashtable->pairs[index].value = value;
-				hashtable->pairs[index].value |= 1u << hashtable_t::shift_valid;
-
-				keys_count++;
-
-				uint64_t longest_chain = try_i + 1;
-				if (this->longest_chain < longest_chain)
-				{
-					this->longest_chain = longest_chain;
-				}
-
-				return eResult::success;
-			}
-			else if (hashtable->is_valid_and_equal(index, key))
-			{
-				hashtable->pairs[index].value = value;
-				hashtable->pairs[index].value |= 1u << hashtable_t::shift_valid;
-
-				rewrites++;
-
-				return eResult::success;
-			}
-		}
-
-		insert_failed++;
-
-		return eResult::isFull;
-	}
-
-public:
-	uint32_t total_size;
-	uint32_t keys_count;
-	std::array<uint32_t, chunk_size + 1> keys_in_chunks;
-	uint32_t longest_chain;
-	uint64_t insert_failed;
-	uint64_t rewrites;
 };
 
 }
